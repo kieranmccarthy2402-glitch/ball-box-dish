6d1b5cad2d6bf5f75623db063e5a52ac66e58deb
kieranmccarthy2402-glitch <kieranmccarthy2402@gmail.com>
2026-05-29 10:14:21 +1200

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,116 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Blue ball":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 8 8 8 . . . . . . . 
+. . . . . 8 8 8 8 8 . . . . . . 
+. . . . 8 8 8 8 8 8 8 . . . . . 
+. . . 8 8 8 8 8 8 8 8 8 . . . . 
+. . . 8 8 8 8 8 8 8 8 8 . . . . 
+. . . 8 8 8 8 8 8 8 8 8 . . . . 
+. . . . 8 8 8 8 8 8 8 . . . . . 
+. . . . . 8 8 8 8 8 . . . . . . 
+. . . . . . 8 8 8 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "Box":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . 4 4 4 4 4 e e 4 4 4 4 4 . . 
+. . 4 4 4 4 4 e e 4 4 4 4 4 . . 
+. . 4 4 4 4 4 e e 4 4 4 4 4 . . 
+. . 4 4 4 4 4 e e 4 4 4 4 4 . . 
+. . 4 4 4 4 4 e e 4 4 4 4 4 . . 
+. . 4 4 4 4 4 e e 4 4 4 4 4 . . 
+. . 4 4 4 4 4 e e 4 4 4 4 4 . . 
+. . 4 4 4 4 4 e e 4 4 4 4 4 . . 
+. . 4 4 4 4 4 e e 4 4 4 4 4 . . 
+. . 4 4 4 4 4 e e 4 4 4 4 4 . . 
+. . 4 4 4 4 4 e e 4 4 4 4 4 . . 
+. . 4 4 4 4 4 e e 4 4 4 4 4 . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "Dish":return img`
+...............bbbbbbbbbbbbbbbbbbb...............
+...........bbbbdd111111111111111ddbbbb...........
+........bbbd1111111111111111111111111dbbb........
+......bbd11111111dddddddddddddd111111111dbb......
+....bbd1111111ddd11111111111111dddd1111111dbb....
+...bd111111ddd111111111111111111111ddd111111db...
+..bd11111ddd111ddddddddddddddddddd111ddd11111db..
+.bd11111dd111dddd111111111111111dddd111dd11111db.
+.b11111d111ddd111111111111111111111ddd111d11111b.
+bd11111d1ddd1111111111111111111111111ddd1111111db
+b11111d1ddd111111111111111111111111111ddd1d11111b
+b11111ddddd111111111111111111111111111ddddd11111b
+b11111ddddd111111111111111111111111111dddbd11111b
+b111111dddd111111111111111111111111111dddb111111b
+bd111111dddd1111111111111111111111111dddbd11111db
+.b1111111dddd11111111111111111111111dddbd111111b.
+.bd1111111dbbdd1111111111111111111dddbbd111111db.
+..bd11111111dbbdd111111111111111dddbbd1111111db..
+...bd111111111dbbbbbbdddddddddddddd111111111db...
+....bbd11111111111dbbbbbbbbbddd11111111111dbb....
+......bbdd11111111111111111111111111111ddbb......
+........bbbdd11111111111111111111111ddbbb........
+...........bbbbbddd11111111111dddbbbbb...........
+................bbbbbbbbbbbbbbbbb................
+`;
+            case "image4":
+            case "Apple":return img`
+. . . . . . . e c 7 . . . . . . 
+. . . . e e e c 7 7 e e . . . . 
+. . c e e e e c 7 e 2 2 e e . . 
+. c e e e e e c 6 e e 2 2 2 e . 
+. c e e e 2 e c c 2 4 5 4 2 e . 
+c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+. e e e 2 2 2 2 2 2 2 2 2 4 e . 
+. 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+. . 2 e e 2 2 2 2 2 4 4 2 e . . 
+. . . 2 2 e e 4 4 4 2 e e . . . 
+. . . . . 2 2 e e e e . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
